aea2c83d83c45637da7e931246d2900b0b3d5fd5
michelececcacci <michelececcacci1@gmail.com>
2021-10-29 19:13:11 +0200

    new function

---
+++ src/vlrggapi/__init__.py
@@ -0,0 +1,6 @@
+from .vlrggapi import get_live_matches
+from .vlrggapi import get_match_by_id
+from .vlrggapi import get_news 
+from .vlrggapi import get_player_matches_by_id
+from .vlrggapi import get_top_n
+from .vlrggapi import get_player_infos
